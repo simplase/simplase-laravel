f74ad2c1401e0b57982ced583013a4c990d8805e
Dmitrii Bobrikov <d.bobrikov@jetmail.cc>
2023-04-08 11:19:01 +0300

    add FeedbackController and Feedback model

---
+++ app/Http/Controllers/API/V1/FeedbackController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Feedback;
+use Illuminate\Http\Request;
+
+class FeedbackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Feedback $feedback)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Feedback $feedback)
+    {
+        //
+    }
+}
